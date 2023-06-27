c5e6ee72849e2c4a822d6bb172dd08424b55522b
danalmeida29 <69275117+danalmeida29@users.noreply.github.com>
2023-06-26 21:38:17 -0300

    AddToDoAndAbout

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Todo extends Component {
+    render(){
+        return(
+            <div>
+                <h1>To-DO</h1>
+            </div>
+        )
+    }
+}
