30c722fc12133019b9e959787d46b82e8d1b43a6
danalmeida29 <69275117+danalmeida29@users.noreply.github.com>
2023-06-27 16:33:44 -0300

    PageHeader

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import PageHeader from '../template/pageHeader'
+
 
 export default class Todo extends Component {
     render(){
         return(
             <div>
-                <h1>To-DO</h1>
+                <PageHeader name='Tarefas' small=' Cadastro'/>
             </div>
         )
     }
